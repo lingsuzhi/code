7bba5e254511cad529643a1fe7c15fda7b2dd713
lingsuzhi <554600654@qq.com>
2019-06-01 18:18:28 +0800

    jys

--- src/main/java/com/lsz/code/code/care/code/java/JavaCode.java
+++ src/main/java/com/lsz/code/code/care/code/java/JavaCode.java
@@ -4,6 +4,6 @@ import com.lsz.code.code.bo.DtoBO;
 
 public interface JavaCode {
     String apply(DtoBO dtoBO);
-    public static final String BasePath ="D:\\code\\xjjd_lease\\lease-apply\\src\\main\\java\\com\\xjjd\\lease\\apply\\api\\base";
-    public static final String BaseXmlPath ="D:\\code\\xjjd_lease\\lease-apply\\src\\main\\resources\\mapper\\base\\";
+    String BasePath ="D:\\github\\ex-jys\\jys-apply\\src\\main\\java\\com\\lsz\\jys\\apply\\base";
+    String BaseXmlPath ="D:\\github\\ex-jys\\jys-apply\\src\\main\\resources\\mapper\\base\\";
 }
