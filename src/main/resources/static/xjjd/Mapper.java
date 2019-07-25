11ab6d602711006180e67de03c841aadbe10fe7c
lingsuzhi <554600654@qq.com>
2019-07-25 13:55:57 +0800

    yl

--- src/main/resources/static/xjjd/Mapper.java
+++ src/main/resources/static/xjjd/Mapper.java
@@ -21,19 +21,44 @@ import java.util.Map;
 @Component("【Lname】MapperBase")
 public interface 【Uname】Mapper {
 
-    //新增
+    /**
+     * 新增
+     *
+     * @param 【Lname】 对象
+     * @return 新增数量
+     */
     Integer add【Uname】(【Uname】 【Lname】);
 
-    //删除
+    /**
+     * 删除
+     *
+     * @param paramMap ids
+     * @return 删除数量
+     */
     Integer delete【Uname】(Map<String, Object> paramMap);
 
-    //查询
+    /**
+     * 查询
+     *
+     * @param paramMap 查询条件
+     * @return 对象
+     */
     【Uname】 get【Uname】(Map<String, Object> paramMap);
 
-    //修改
+    /**
+     * 修改
+     *
+     * @param 【Lname】 对象
+     * @return 修改数量
+     */
     Integer update【Uname】(【Uname】 【Lname】);
 
-    //分页查询
+    /**
+     * 分页查询
+     *
+     * @param paramMap 查询条件
+     * @return 查询列表
+     */
     List<【Uname】> get【Uname】List(Map<String, Object> paramMap);
 }
 
