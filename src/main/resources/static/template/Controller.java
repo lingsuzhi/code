12359261fa271ab339be6241425dd3737a0aaf23
Administrator <554600654@qq.com>
2019-11-10 17:23:22 +0800

    0406

--- src/main/resources/static/template/Controller.java
+++ src/main/resources/static/template/Controller.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 import java.util.List;
@@ -37,7 +38,7 @@ public class 【Uname】Controller {
     * 基础功能：查询数据列表
     */
     @RequestMapping(value = "/get【Uname】List", method = RequestMethod.POST)
-    public ResultVO<List<Map<String, Object>>> get【Uname】List(Map<String, Object> paramMap){
+    public ResultVO<List<Map<String, Object>>> get【Uname】List(@RequestBody Map<String, Object> paramMap){
         return  ResultVOUtil.success(【Lname】ServiceImpl.get【Uname】List(paramMap));
     }
 
@@ -45,7 +46,7 @@ public class 【Uname】Controller {
     * 基础功能：分页查询数据列表
     */
     @RequestMapping(value = "/get【Uname】PagingList", method = RequestMethod.POST)
-    public ResultVO<Map<String, Object>> get【Uname】PagingList(Map<String, Object> paramMap){
+    public ResultVO<Map<String, Object>> get【Uname】PagingList(@RequestBody Map<String, Object> paramMap){
         return  ResultVOUtil.success(【Lname】ServiceImpl.get【Uname】PagingList(paramMap));
     }
 
@@ -53,7 +54,7 @@ public class 【Uname】Controller {
     * 基础功能：查询单条数据
     */
     @RequestMapping(value = "/get【Uname】", method = RequestMethod.POST)
-    public ResultVO<Map<String, Object>> get【Uname】(Map<String, Object> paramMap){
+    public ResultVO<Map<String, Object>> get【Uname】(@RequestBody Map<String, Object> paramMap){
         return  ResultVOUtil.success(【Lname】ServiceImpl.get【Uname】(paramMap));
     }
 
@@ -61,7 +62,7 @@ public class 【Uname】Controller {
     * 基础功能：保存数据
     */
     @RequestMapping(value = "/save【Uname】Record", method = RequestMethod.POST)
-    public ResultVO save【Uname】Record(Map<String, Object> paramMap){
+    public ResultVO save【Uname】Record(@RequestBody Map<String, Object> paramMap){
         return  ResultVOUtil.success(【Lname】ServiceImpl.save【Uname】Record(paramMap));
     }
 
@@ -69,7 +70,7 @@ public class 【Uname】Controller {
     * 基础功能：修改数据
     */
     @RequestMapping(value = "/update【Uname】Record", method = RequestMethod.POST)
-    public ResultVO update【Uname】Record(Map<String, Object> paramMap){
+    public ResultVO update【Uname】Record(@RequestBody Map<String, Object> paramMap){
         return  ResultVOUtil.success(【Lname】ServiceImpl.update【Uname】Record(paramMap));
     }
 
@@ -77,7 +78,7 @@ public class 【Uname】Controller {
     * 基础功能：逻辑删除数据
     */
     @RequestMapping(value = "/delete【Uname】Record", method = RequestMethod.POST)
-    public ResultVO delete【Uname】Record(Map<String, Object> paramMap){
+    public ResultVO delete【Uname】Record(@RequestBody Map<String, Object> paramMap){
         return  ResultVOUtil.success(【Lname】ServiceImpl.delete【Uname】Record(paramMap));
     }
 
@@ -85,7 +86,7 @@ public class 【Uname】Controller {
     * 基础功能：物理删除数据
     */
     @RequestMapping(value = "/remove【Uname】Record", method = RequestMethod.POST)
-    public ResultVO remove【Uname】Record(Map<String, Object> paramMap){
+    public ResultVO remove【Uname】Record(@RequestBody Map<String, Object> paramMap){
         return  ResultVOUtil.success(【Lname】ServiceImpl.remove【Uname】Record(paramMap));
     }
 }
